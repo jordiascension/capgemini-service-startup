62e54f445f0c67f87438d3fa3936b88cce97c55a
jordiascension <jordiascension@enterpriseconsulting.es>
2022-06-08 17:38:28 +0200

    Added Test End to End with postman environment file.

--- postman/TestE2E.bat
+++ postman/TestE2E.bat
@@ -1,2 +1,4 @@
 call newman run capgemini-service.postman_collection.json -e Capgemini_Calculator_dev.postman_environment.json
 call newman run capgemini-service-pension-plan.postman_collection.json -e Capgemini_Calculator_dev.postman_environment.json
+call newman run capgemini-service.postman_collection.json -e Capgemini_Calculator_pro2.postman_environment.json
+call newman run capgemini-service-pension-plan.postman_collection.json -e Capgemini_Calculator_pro2.postman_environment.json
